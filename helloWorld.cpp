b5261373b12e63b5710ca4716a72b7da60480ca4
Gabriel Vallarta <19050@portalcedi.edu.mx>
2024-01-30 13:44:48 -0600

    rama

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -2,7 +2,14 @@
 using namespace std;
 
 int main(){
-
-    cout<<"hello world"<<endl;
+    int x;
+    cout<<"cual es tu edad?"<<endl;
+    cin>>x;
+    if(x<18){
+        cout<<"eres menor de edad"<<endl;
+    }else
+    {
+        cout<<"eres mayor de edad"<<endl;
+    }
     return 0;
 }
